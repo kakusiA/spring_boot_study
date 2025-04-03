3956509517044db4fc01f325f192e3fd372c7c2f
kakusi <wtecho331@gmail.com>
2025-04-03 17:48:21 +0900

    get 추가

--- src/main/java/com/jast/javastudy/JavaStudyApplication.java
+++ src/main/java/com/jast/javastudy/JavaStudyApplication.java
@@ -2,12 +2,19 @@ package com.jast.javastudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JavaStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaStudyApplication.class, args);
+
+    }
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "hello";
     }
 
 }
